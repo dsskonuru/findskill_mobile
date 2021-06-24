9fbb745c51d00375c7f892b8773c9eb270a49ed4
dheerajv09 <dheerajv634@gmail.com>
2021-06-24 10:06:23 +0530

    created refer and earn ui

--- lib/features/job-seeker-module/refer-and-earn/presentation/pages/refer_and_earn_page.dart
+++ lib/features/job-seeker-module/refer-and-earn/presentation/pages/refer_and_earn_page.dart
@@ -1,1 +1,239 @@
 // TODO: Build the UI along with appropriate tests for Refer and Earn Page @dheerajv09
+
+import 'package:find_skill/features/onboarding/presentation/widgets/app_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+
+class ReferAndEarn extends StatefulWidget {
+  const ReferAndEarn({Key? key}) : super(key: key);
+
+  @override
+  _ReferAndEarnState createState() => _ReferAndEarnState();
+}
+
+class _ReferAndEarnState extends State<ReferAndEarn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: FindSkillAppBar(),
+        body: SafeArea(
+          child: Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 5.4.w),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 4.h,
+                  ),
+                  Text(
+                    "Refer & Earn",
+                    style: Theme.of(context).textTheme.headline5!.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                  SizedBox(
+                    height: 1.5.h,
+                  ),
+                  Container(
+                    height: 11.h,
+                    padding: EdgeInsets.symmetric(horizontal: 4.w),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.secondaryVariant,
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Invite your friends to join FINDSKILL & Earn Continously",
+                        style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500
+                            ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 7.h,
+                  ),
+                  SizedBox(
+                    height: 26.h,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text(
+                                "Your Referral ID",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline6!
+                                    .copyWith(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                    ),
+                              ),
+                              SizedBox(
+                                height: 9.h,
+                              ),
+                              SizedBox(
+                                height: 5.7.h,
+                                child: Directionality(
+                                  textDirection: TextDirection.rtl,
+                                  child: ElevatedButton.icon(
+                                    onPressed: () {},
+                                    //TODO: have to add original icon
+                                    icon: const Icon(Icons.copy),
+                                    label: const Text("Invite"),
+                                    style: ButtonStyle(
+                                      backgroundColor:
+                                          MaterialStateProperty.all(
+                                        const Color.fromRGBO(82, 100, 109, 1),
+                                      ),
+                                      shape: MaterialStateProperty.all<
+                                          RoundedRectangleBorder>(
+                                        RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(9.0),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 1.h,
+                              ),
+                              SizedBox(
+                                height: 5.7.h,
+                                width: 26.w,
+                                child: Directionality(
+                                  textDirection: TextDirection.rtl,
+                                  child: ElevatedButton.icon(
+                                      onPressed: () {},
+                                      icon: const Icon(Icons.copy),
+                                      label: const Text("Copy"),
+                                      style: ButtonStyle(
+                                        backgroundColor:
+                                            MaterialStateProperty.all(
+                                          const Color.fromRGBO(82, 100, 109, 1),
+                                        ),
+                                        shape: MaterialStateProperty.all<
+                                            RoundedRectangleBorder>(
+                                          RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(9.0),
+                                          ),
+                                        ),
+                                      )),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            color: Colors.black,
+                            child: Center(
+                              child: Text(
+                                "QR CODE",
+                                textAlign: TextAlign.center,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline3!
+                                    .copyWith(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5.h,
+                  ),
+                  ClipRRect(
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                    child: Container(
+                      height: 11.h, //100,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.secondaryVariant,
+                      ),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 4,
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 5.w, vertical: 4),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "You have Earned",
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headline6!
+                                        .copyWith(
+                                            fontWeight: FontWeight.bold,
+                                            letterSpacing: 0,
+                                            fontSize: 19,
+                                            height: 2),
+                                  ),
+                                  Text(
+                                    "Rs. 1000",
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headline6!
+                                        .copyWith(
+                                          fontSize: 35,
+                                          color: Colors.grey,
+                                          fontWeight: FontWeight.bold,
+                                          height: 1,
+                                        ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: Material(
+                              child: InkWell(
+                                onTap: () {
+                                },
+                                splashColor: Colors.white,
+                                focusColor: Colors.white,
+                                child: Container(
+                                  color: Colors.black,
+                                  child: Center(
+                                    child: Text(
+                                      "Redeem",
+                                      textAlign: TextAlign.center,
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodyText1!
+                                          .copyWith(color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
