1a2d642a4a115c1a050bc77058dd17dea18af3a0
Dheeraj Verma <dheerajv634@gmail.com>
2021-07-06 10:57:05 +0530

    can switch between menu button or back button

--- lib/features/onboarding/presentation/widgets/app_bar.dart
+++ lib/features/onboarding/presentation/widgets/app_bar.dart
@@ -1,22 +1,29 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:find_skill/core/theme/theme_data.dart';
-import 'package:find_skill/features/onboarding/data/models/language.dart';
-import 'package:find_skill/features/onboarding/presentation/pages/language_page.dart';
-import 'package:find_skill/features/onboarding/presentation/provider/language_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
-class FindSkillAppBar extends ConsumerWidget implements PreferredSizeWidget {
+import 'package:find_skill/features/job-seeker-module/navigation_bar/presentation/model/drawer_item.dart';
+
+class FindSkillAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final bool enableMenu;
+  final VoidCallback? onsSelectedItem;
+  const FindSkillAppBar({
+    Key? key,
+    this.enableMenu = false,
+    this.onsSelectedItem,
+  })  : assert((enableMenu == false && onsSelectedItem == null) ||
+            (enableMenu == true && onsSelectedItem != null)),
+        super(key: key);
+
   @override
   Size get preferredSize => const Size.fromHeight(180);
 
   @override
-  Widget build(BuildContext context, ScopedReader watch) {
+  Widget build(BuildContext context) {
     final double _statusbarHeight = MediaQuery.of(context).padding.top;
     return Container(
-      height: 14.h,
+      height: 13.5.h,
       padding: EdgeInsets.only(
         top: _statusbarHeight,
         right: 7.w,
@@ -24,7 +31,10 @@ class FindSkillAppBar extends ConsumerWidget implements PreferredSizeWidget {
       ),
       decoration: BoxDecoration(
         gradient: LinearGradient(
-            colors: [vandylBlue, scubaBlue],
+            colors: [
+              Theme.of(context).primaryColor,
+              Theme.of(context).secondaryHeaderColor,
+            ],
             begin: const FractionalOffset(0.0, 0.0),
             end: const FractionalOffset(0.5, 0.0),
             stops: const [0.0, 1.0]),
@@ -41,14 +51,17 @@ class FindSkillAppBar extends ConsumerWidget implements PreferredSizeWidget {
             ),
             child: IconButton(
               color: Colors.white,
-              onPressed: () => context.router.pop(),
-              icon: const Icon(Icons.arrow_back_ios_new),
+              onPressed:
+                  enableMenu ? onsSelectedItem : () => context.router.pop(),
+              icon: enableMenu
+                  ? const Icon(Icons.menu)
+                  : const Icon(Icons.arrow_back_ios_new),
               iconSize: 3.w,
             ),
           ),
           SvgPicture.asset(
             'assets/svg/findskill-appbar.svg',
-            height: 7.h,
+            height: 7.5.h,
           ),
           Container(
             height: 9.w,
@@ -57,29 +70,17 @@ class FindSkillAppBar extends ConsumerWidget implements PreferredSizeWidget {
               borderRadius: BorderRadius.circular(1.w),
               color: Colors.white,
             ),
-            child: PopupMenuButton<String>(
-              icon: Text(
-                watch(languageProvider).language.code.toUpperCase(),
+            child: TextButton(
+              onPressed: () {
+                //TODO: Add Option to change language
+              },
+              child: Text(
+                'EN',
                 style: TextStyle(
                   color: Theme.of(context).primaryColor,
-                  fontSize: 2.5.w,
+                  fontSize: 3.w,
                 ),
               ),
-              itemBuilder: (BuildContext context) {
-                final LanguagesList _languages =
-                    watch(languageProvider).languages;
-                return _languages.list
-                    .map((_language) => PopupMenuItem<String>(
-                          value: _language.name,
-                          child: Text(
-                            _language.name,
-                            style: Theme.of(context).textTheme.overline,
-                          ),
-                        ))
-                    .toList();
-              },
-              onSelected: (_languageName) =>
-                  changeLanguage(_languageName, context),
             ),
           ),
         ],
